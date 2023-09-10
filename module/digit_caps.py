ad427f22a527ff57c5281b815854b315121cdc72
Lioe <72618349+Lioely@users.noreply.github.com>
2023-09-10 15:55:54 +0800

    Update digit_caps.py

--- module/digit_caps.py
+++ module/digit_caps.py
@@ -1,1 +1,74 @@
+import torch.nn as nn
+import torch
+
+
+def squash(x, dim=-1):
+    squared_norm = (x ** 2).sum(dim=dim, keepdim=True)
+    scale = squared_norm / (1 + squared_norm)
+    return scale * x / (squared_norm.sqrt() + 1e-8)
+
+
+class DigitCaps(nn.Module):
+    """Digit capsule layer."""
+
+    def __init__(self, in_dim, in_caps, num_caps, dim_caps, num_routing, device='cuda'):
+        """
+        Initialize the layer.
+
+        Args:
+            in_dim: 		Dimensionality (i.e. length) of each capsule vector.
+            in_caps: 		Number of input capsules if digits layer.
+            num_caps: 		Number of capsules in the capsule layer
+            dim_caps: 		Dimensionality, i.e. length, of the output capsule vector.
+            num_routing:	Number of iterations during routing algorithm
+        """
+        super(DigitCaps, self).__init__()
+        self.in_dim = in_dim
+        self.in_caps = in_caps
+        self.num_caps = num_caps
+        self.dim_caps = dim_caps
+        self.num_routing = num_routing
+        self.device = device
+        self.W = nn.Parameter(0.01 * torch.randn(1, num_caps, in_caps, dim_caps, in_dim),
+                              requires_grad=True)
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        # unsqueeze-> (batch_size, in_caps, in_dim) -> (batch_size, 1, in_caps, in_dim, 1)
+        x = x.unsqueeze(1).unsqueeze(4)
+        # W @ x =
+        # (1, num_caps, in_caps, dim_caps, in_dim) @ (batch_size, 1, in_caps, in_dim, 1) =
+        # (batch_size, num_caps, in_caps, dim_caps, 1)
+        u_hat = torch.matmul(self.W, x)
+        # (batch_size, num_caps, in_caps, dim_caps)
+        u_hat = u_hat.squeeze(-1)
+        # detach u_hat during routing iterations to prevent gradients from flowing
+        temp_u_hat = u_hat.detach()
+
+        b = torch.zeros(batch_size, self.num_caps, self.in_caps, 1).to(self.device)
+
+        for route_iter in range(self.num_routing - 1):
+            # (batch_size, num_caps, in_caps, 1) -> Softmax along num_caps
+            c = b.softmax(dim=1)
+
+            # element-wise multiplication
+            # (batch_size, num_caps, in_caps, 1) * (batch_size, in_caps, num_caps, dim_caps) ->
+            # (batch_size, num_caps, in_caps, dim_caps) sum across in_caps ->
+            # (batch_size, num_caps, dim_caps)
+            s = (c * temp_u_hat).sum(dim=2)
+            # apply "squashing" non-linearity along dim_caps
+            v = squash(s)
+            # dot product agreement between the current output vj and the prediction uj|i
+            # (batch_size, num_caps, in_caps, dim_caps) @ (batch_size, num_caps, dim_caps, 1)
+            # -> (batch_size, num_caps, in_caps, 1)
+            uv = torch.matmul(temp_u_hat, v.unsqueeze(-1))
+            b += uv
+
+        # last iteration is done on the original u_hat, without the routing weights update
+        c = b.softmax(dim=1)
+        s = (c * u_hat).sum(dim=2)
+        # apply "squashing" non-linearity along dim_caps
+        v = squash(s)
+
+        return v
 
